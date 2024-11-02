70b069f77412ef5c76730ad637c0a74300cb40b3
Sumit Chavan <sumit.chavan@cognustechnology.com>
2024-11-02 15:33:33 +0530

    minor chnages
    Done

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const colors = require("colors");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.Mongo_URL);
+    await mongoose.connect("mongodb+srv://sdchavan8070:Shubham%40123@schavan.za0uwda.mongodb.net/meadhikari");
     console.log(`Connected to Server ${mongoose.connection.host}`.bgCyan.white);
   } catch (error) {
     console.log(`Database Connection Error ${error}`.bgRed.white);
