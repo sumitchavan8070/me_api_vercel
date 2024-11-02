bbbc50934d047891cf0db69c55304de0bc35b0d1
Sumit Chavan <sumit.chavan@cognustechnology.com>
2024-11-02 15:05:39 +0530

    Chnages
    Minor Changes

--- server.js
+++ server.js
@@ -105,44 +105,44 @@ chatSocket(io);
 
 //custome route
 app.use("/api/v1/auth", cacheMiddleware, require("./routes/userRoutes"));
-app.use("/api/v1/admin", require("./routes/adminRoutes"));
-app.use("/api/v1/admin", require("./routes/studentsTableBackedRoute"));
-app.use("/api/v1/admin/donation", require("./routes/donationRoutes"));
-app.use("/api/v1/auth/donation", require("./routes/donationRoutes"));
+app.use("/api/v1/admin", cacheMiddleware, require("./routes/adminRoutes"));
+app.use("/api/v1/admin",cacheMiddleware,  require("./routes/studentsTableBackedRoute"));
+app.use("/api/v1/admin/donation", cacheMiddleware, require("./routes/donationRoutes"));
+app.use("/api/v1/auth/donation",cacheMiddleware, require("./routes/donationRoutes"));
 
-app.use("/api/v1/auth/posts", require("./routes/postRoutes"));
-app.use("/api/v1/admin/posts", require("./routes/postRoutes"));
+app.use("/api/v1/auth/posts",cacheMiddleware,  require("./routes/postRoutes"));
+app.use("/api/v1/admin/posts", cacheMiddleware, require("./routes/postRoutes"));
 
-app.use("/api/v1/auth/exam-categories", require("./routes/examCategoryRoutes")); //DONE
-app.use("/api/v1/auth/subcategories", require("./routes/subExamTypeRoutes")); // Done
-app.use("/api/v1/admin/subcategories", require("./routes/subExamTypeRoutes")); // Done
+app.use("/api/v1/auth/exam-categories", cacheMiddleware, require("./routes/examCategoryRoutes")); //DONE
+app.use("/api/v1/auth/subcategories", cacheMiddleware, require("./routes/subExamTypeRoutes")); // Done
+app.use("/api/v1/admin/subcategories", cacheMiddleware, require("./routes/subExamTypeRoutes")); // Done
 
-app.use("/api/v1/auth/years", require("./routes/examYearRoutes")); // Done , for frontend
+app.use("/api/v1/auth/years", cacheMiddleware, require("./routes/examYearRoutes")); // Done , for frontend
 app.use(
   "/api/v1/auth/question-papers",
-  require("./routes/questionPaperRoutes")
+  cacheMiddleware, require("./routes/questionPaperRoutes")
 );
 app.use(
   "/api/v1/admin/question-papers",
-  require("./routes/questionPaperRoutes")
+  cacheMiddleware, require("./routes/questionPaperRoutes")
 );
 
-app.use("/api/v1/auth/subjects", require("./routes/subjectRoutes")); // Include subject routes
+app.use("/api/v1/auth/subjects",cacheMiddleware, require("./routes/subjectRoutes")); // Include subject routes
 
-app.use("/api/v1/auth/groups", require("./routes/groupRoutes"));
+app.use("/api/v1/auth/groups", cacheMiddleware, require("./routes/groupRoutes"));
 
 //leaderboard
-app.use("/api/v1/auth/leaderboard", require("./routes/leaderboardRoutes"));
+app.use("/api/v1/auth/leaderboard", cacheMiddleware, require("./routes/leaderboardRoutes"));
 
-app.use("/api/v1/auth/customtest", require("./routes/customTestRoutes"));
+app.use("/api/v1/auth/customtest",cacheMiddleware,  require("./routes/customTestRoutes"));
 
-app.use("/api/v1/auth/papers", require("./routes/allPaperRoutes"));
-app.use("/api/v1/admin/papers", require("./routes/allPaperRoutes"));
+app.use("/api/v1/auth/papers", cacheMiddleware, require("./routes/allPaperRoutes"));
+app.use("/api/v1/admin/papers", cacheMiddleware, require("./routes/allPaperRoutes"));
 
-app.use("/api/v1/auth/feedback", require("./routes/feedbackRoutes"));
-app.use("/api/v1/admin/feedback", require("./routes/feedbackRoutes"));
+app.use("/api/v1/auth/feedback", cacheMiddleware, require("./routes/feedbackRoutes"));
+app.use("/api/v1/admin/feedback", cacheMiddleware, require("./routes/feedbackRoutes"));
 
-app.use("/api/v1/auth/abc", require("./routes/examDetailWithYearRoute"));
+app.use("/api/v1/auth/abc", cacheMiddleware, require("./routes/examDetailWithYearRoute"));
 
 //port
 const PORT = process.env.PORT || 2020;
@@ -158,7 +158,7 @@ server.listen(PORT, () => {
 
 // server added to vercel
 
-// app.get("/", (req, res) => {
-//   res.send("Server is running!");
-//   console.log("Root path accessed. Server is running.".bgGreen.white);
-// });
+app.get("/", (req, res) => {
+  res.send("Api is running ready to start !");
+  console.log("Root path accessed. Server is running.".bgGreen.white);
+});
